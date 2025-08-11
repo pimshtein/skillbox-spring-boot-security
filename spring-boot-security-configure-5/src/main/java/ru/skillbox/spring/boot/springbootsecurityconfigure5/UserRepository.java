253bb73133ee28c8942afb82d73cc92aafacd866
v.spiridonov <v.spiridonov@podeli.ru>
2025-08-11 09:24:38 +0300

    Исправления

--- spring-boot-security-configure-5/src/main/java/ru/skillbox/spring/boot/springbootsecurityconfigure5/UserRepository.java
+++ spring-boot-security-configure-5/src/main/java/ru/skillbox/spring/boot/springbootsecurityconfigure5/UserRepository.java
@@ -1,8 +1,25 @@
 package ru.skillbox.spring.boot.springbootsecurityconfigure5;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Map;
 import java.util.Optional;
+import java.util.HashMap;
 
-public interface UserRepository extends JpaRepository<UsersEntity, Long> {
-    Optional<UsersEntity> findByUsername(String username);
+@Repository
+public class UserRepository {
+    
+    private final Map<String, UsersEntity> users = new HashMap<>();
+    
+    public UserRepository() {
+        users.put("admin", new UsersEntity(1L, "admin",
+            "$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", "ROLE_ADMIN"));
+        users.put("user", new UsersEntity(3L, "user",
+            "$2a$10$5XvZGA6dWUJv.FTDExu0iu0Uhh6WVMzpwZFGCH3wNbNvpwaHKrI0q", "ROLE_USER"));
+        users.put("manager", new UsersEntity(3L, "manager",
+            "$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", "ROLE_MANAGER"));
+    }
+    
+    public Optional<UsersEntity> findByUsername(String username) {
+        return Optional.ofNullable(users.get(username));
+    }
 } 
--- spring-boot-security-configure-5/src/main/java/ru/skillbox/spring/boot/springbootsecurityconfigure5/UsersEntity.java
+++ spring-boot-security-configure-5/src/main/java/ru/skillbox/spring/boot/springbootsecurityconfigure5/UsersEntity.java
@@ -1,18 +1,11 @@
 package ru.skillbox.spring.boot.springbootsecurityconfigure5;
 
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
-@Entity
-@Table(name = "users")
 @Data
+@AllArgsConstructor
 public class UsersEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String username;
     String password;
